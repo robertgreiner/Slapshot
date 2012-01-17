f978c183929c2191b8e436f847e3f908f5410c38
Robert Greiner <robert@robertgreiner.com>
2012-01-16 18:21:24 -0600

    Save screenshots to a file for later use.

---
+++ Slapshot/Slapshot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Slapshot
+{
+    public partial class Slapshot : Form
+    {
+        public Slapshot()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            CaptureEntireScreen();    
+        }
+        
+        private void CaptureEntireScreen()
+        {
+            var bounds = Screen.GetBounds(Point.Empty);
+            ProcessBitmap(bounds);
+        }
+
+        private void CaptureActiveScreen()
+        {
+            var bounds = this.Bounds;
+            ProcessBitmap(bounds);
+        }
+
+        private void ProcessBitmap(Rectangle rectangle)
+        {
+            using (var image = new Bitmap(rectangle.Width, rectangle.Height))
+            {
+                using (var g = Graphics.FromImage(image))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, rectangle.Size);
+                }
+                SaveImage(image);
+            }
+        }
+
+        private static void SaveImage(Bitmap image)
+        {
+            image.Save("screenshot.png", ImageFormat.Png);
+        }
+    }
+}
